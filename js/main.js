313dd400509aca5090ece548e77f7ba24c164e03
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-17 16:44:05 +0100

    1-rel

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var name =prompt("Qual è il tuo nome?");
+var surname =prompt("Qual è il tuo cognome?");
+var favouriteColor =prompt("Qual è il tuo colore preferito?");
+document.getElementById('psswd-gen').innerHTML = name + surname + favouriteColor + 20;
