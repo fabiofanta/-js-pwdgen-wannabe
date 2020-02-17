f0399bc980d79673996161d300db5a1670622fd4
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-17 17:15:41 +0100

    3-rel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var name =prompt("Qual è il tuo nome?");
-var surname =prompt("Qual è il tuo cognome?");
-var favouriteColor =prompt("Qual è il tuo colore preferito?");
-document.getElementById('text').innerHTML = name + surname + favouriteColor + 20;
+var name = prompt("Qual è il tuo nome?");
+var surname = prompt("Qual è il tuo cognome?");
+var favouriteColor = prompt("Qual è il tuo colore preferito?");
+var date = new Date();
+var year = date.getFullYear();
+// document.getElementById('text').innerHTML = name + surname + favouriteColor + 20;
+// document.getElementById('text').innerHTML = name + surname + favouriteColor + year;
+// document.getElementById('text').innerHTML = name + ";" + surname + ";" + favouriteColor + ";" + year;
+document.getElementById('text').innerHTML = name + ";" + surname + ";" + favouriteColor + ";" + year;
